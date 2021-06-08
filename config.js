d7f674ca21f06dd3800c55e4c82b828e4bd75ab0
xuvado789 <85499911+xuvado789@users.noreply.github.com>
2021-06-08 09:11:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối 21/6 anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100033763668303' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
